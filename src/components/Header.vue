93bd32b84eb0b37d5f3a01d11fa32bb44f9a8cdf
TeeJay007 <17435461+TeeJay007@users.noreply.github.com>
2021-09-30 03:36:04 +0300

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav>
+      <div class="p-4 border-b flex items-center space-x-2">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7" />
+          </svg>
+        </div>
+        <div>
+          <h1 class="font-bold text-3xl">Parkai</h1>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
