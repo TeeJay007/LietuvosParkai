4d58e9b2dd6d91d5e34d99a6327bdc1c70f3f7cc
TeeJay007 <17435461+TeeJay007@users.noreply.github.com>
2021-09-30 03:36:20 +0300

    Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+let timerID: number = 0;
+
+const emit =
+  defineEmits<{
+    (e: "on-search", search: string): void;
+  }>();
+
+const search = ref("");
+const onSearch = () => {
+  clearTimeout(timerID);
+  timerID = setTimeout(() => {
+    emit("on-search", search.value.trim().toLowerCase());
+  }, 500);
+};
+</script>
+<template>
+  <div>
+    <input
+      @input="onSearch"
+      v-model="search"
+      class="p-4 border-b w-full"
+      type="text"
+      placeholder="Ieškoti..."
+    />
+  </div>
+</template>
